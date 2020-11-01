045236c0a88a5b0d3caef4564e379e6d1cf1c999
Michael <mdzhang2018@gmail.com>
2020-11-01 16:10:52 -0500

    New app randomization

--- scripts.js
+++ scripts.js
@@ -1,19 +1,60 @@
 $(document).ready(() => {
+
+    function randomLetter() {
+        var text = "";
+        var possible = "abcdefghijklmnopqrstuvwxyz";
+        for (var i = 0; i < 1; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    }
+
+    function randomNumber(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    $('#root').append(`<div id="options"></div>`);
+    $('#options').append(`<p>Type of meal</p>`);
+    $('#options').append(`<input type="checkbox" id="option1" name="meal_type" value="snack">`);
+    $('#options').append(`<label for="option1">Snack</label><br>`);
+    $('#options').append(`<input type="checkbox" id="option2" name="meal_type" value="breakfast">`);
+    $('#options').append(`<label for="option2">Breakfast</label><br>`);
+    $('#options').append(`<input type="checkbox" id="option3" name="meal_type" value="lunch">`);
+    $('#options').append(`<label for="option3">Lunch</label><br>`);
+    $('#options').append(`<input type="checkbox" id="option4" name="meal_type" value="dinner">`);
+    $('#options').append(`<label for="option4">Dinner</label><br>`);
     $('#name_button').on('click', (e) => {
+        let query = "https://api.edamam.com/search?q=" + randomLetter() + "&app_id=f4712c78&app_key=dbc1658d4964a9a2a6ac12abd88d4b81";
+        query = query + "&health=alcohol-free"
         $.get({
-            url: "https://api.edamam.com/search?q=chicken&app_id=f4712c78&app_key=dbc1658d4964a9a2a6ac12abd88d4b81&from=0&to=3&calories=591-722&health=alcohol-free",
+            url: query,
+            // &app_id=f4712c78&app_key=dbc1658d4964a9a2a6ac12abd88d4b81&calories=500-600&health=alcohol-free
             success: (result) => {
                 let allRecipes = result.hits;
-                allRecipes.forEach(recipe => {
+                if (allRecipes.length == 0) {
+                    $('#recipe_list').append(`<li>No results</li>`);
+                }
+                else {
+                    let randomNum = randomNumber(allRecipes.length);
+                    let recipe = allRecipes[randomNum];
                     let generalInfo = recipe["recipe"];
                     let calories = generalInfo["calories"];
                     let name = generalInfo["label"];
                     let imageLink = generalInfo["image"];
                     console.log("Name = " + name + "  Calories = " + calories + "  Image_Link = " + imageLink);
-                    $('#recipe_list').append(`<li>Name: ${name}  Calories: ${calories}</li>`);
-                    $('#recipe_list').append(`<li><img src=${imageLink} style="width: 400px; height: 400px;"><li>`);
-                })
-                console.log(console.log(result.hits[0]["recipe"]));
+                    $('#recipe_div').empty().append(`<p>Name: ${name}  Calories: ${calories}<p>`);
+                    $('#recipe_div').append(`<p><img src=${imageLink} style="width: 400px; height: 400px;"><p>`);
+                    /*
+                    allRecipes.forEach(recipe => {
+                        let generalInfo = recipe["recipe"];
+                        let calories = generalInfo["calories"];
+                        let name = generalInfo["label"];
+                        let imageLink = generalInfo["image"];
+                        console.log("Name = " + name + "  Calories = " + calories + "  Image_Link = " + imageLink);
+                        $('#recipe_list').append(`<li>Name: ${name}  Calories: ${calories}</li>`);
+                        $('#recipe_list').append(`<li><img src=${imageLink} style="width: 400px; height: 400px;"><li>`);
+                    })
+                    */
+                }
             },
             error: () => {
                 console.log("Failed!");
