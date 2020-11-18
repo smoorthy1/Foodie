690b483c9b141c576e0dd095d4d9810d47bee8de
Michael <mdzhang2018@gmail.com>
2020-11-18 11:07:06 -0500

    Took out first and last name fields

--- inboxView.js
+++ inboxView.js
@@ -80,26 +80,7 @@ $(document).ready(() => {
             $('#greeting').text(`Hello, Not signed in`);
         }
     });
-
-    const $root = $('#root');
-    const $page = $('<div id="page"><div>').addClass('main');
-    $page.append(header(), footer());
-    $root.append(sideBar(), $page);
-
-
-    $('#sidebar').on('mouseover', function(event) {
-        event.preventDefault();
-        document.getElementById('sidebar').style.width = '250px';
-        document.getElementById('page').style.marginLeft = '250px'; 
-    });
-
-    $('#sidebar').on('mouseout', function(event) {
-        event.preventDefault();
-        document.getElementById('sidebar').style.width = '85px';
-        document.getElementById('page').style.marginLeft = '85px'; 
-    });   
-
-    /*
+    
     auth.onAuthStateChanged(function(user) {
         if(user) {
             window.authUID = firebase.auth().currentUser.uid;
@@ -128,7 +109,7 @@ $(document).ready(() => {
             alert("No Active User");
         }
     });
-    */
+    
     if ($('#root').is('empty')) {
         alert("empty page");
     }
